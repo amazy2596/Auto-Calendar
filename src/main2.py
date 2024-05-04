5d9811aebd1dd7837983c41328808110d86b35ee
51Hz <1512317895@qq.com>
2024-05-04 18:01:08 +0800

    An alternative startup method is used (#1)

---
+++ src/main2.py
@@ -0,0 +1,8 @@
+import auto_calendar
+
+instance1 = auto_calendar.auto_calendar()
+
+instance1.get_luogu_contest()
+instance1.get_atcoder_contest()
+instance1.get_nowcoder_contest()
+instance1.get_codeforces_contest()
