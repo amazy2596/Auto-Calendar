faae14278bd74b2509bf85f4f83f7eaccbf29794
amazy <fanhengjia2596@outlook.com>
2024-05-10 00:03:29 +0800

    change codeforces to only add Div contests

--- src/auto_calendar.py
+++ src/auto_calendar.py
@@ -45,7 +45,10 @@ class auto_calendar:
             cols = row.find_all('td')
             time = cols[2].get_text()
             name = cols[0].get_text().replace("\r\n                \n\r\n                            Enter »\r\n                    \n\n", ' ').strip()
-            operatorr.auto_calendar(name, time, 'codeforces', self.positions)
+            if re.search(r'\(Div. \d\)', name) is not None:
+                if re.search(r'Round \d+ \(Div. \d\)', name) is not None:
+                    operatorr.auto_calendar(name, time, 'codeforces', self.positions)
+            
 
     def get_nowcoder_contest(self):
         url = "https://ac.nowcoder.com/acm/contest/vip-index"
